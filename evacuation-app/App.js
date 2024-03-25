cac37ba5418b6f4eec098355de8672d85cfe8ff3
nnalijm <naaaaaaaaaaaad@gmail.com>
2024-03-25 19:13:51 +0100

    Project initial commit

---
+++ evacuation-app/App.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>
+        Here is some text
+      </Text>
+    </SafeAreaView>
+  );
+}
