04950966d6d55664a55b7711ec02da88c9f964f3
nnalijm <naaaaaaaaaaaad@gmail.com>
2024-03-25 20:02:42 +0100

    Add LogicScreen

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import LoginScreen from './Screens/LoginScreen';
+import HomeScreen from './Screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>
-        Here is some text
-      </Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <LoginScreen/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
